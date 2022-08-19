729b6d3585a40b28d6d4447a89ea290b22468248
Weslley Araujo <weslleyaraujo@weslleys-air.lan>
2022-08-19 19:35:58 +0200

    [init] initial commit

---
+++ Shared/Park_and_RideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Park_and_RideApp.swift
+//  Shared
+//
+//  Created by Weslley Araujo on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Park_and_RideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
